a89264976d7df242748c26ac862e1548b278188f
wk-j <somnuk.wk@gmail.com>
2019-07-10 12:35:01 +0700

    Finder

---
+++ src/ScriptFinder/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ScriptFinder {
+    class Program {
+
+        static void Main(string[] args) {
+            var asm = typeof(KFontend.KFrontendClass).Assembly;
+            foreach (var item in asm.GetManifestResourceNames()) {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
